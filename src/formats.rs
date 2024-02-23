2b4fa8d01e7e1e080f1af9db47240238f6834053
Milan Jovanovic <milanj@gmail.com>
2024-02-23 14:59:45 +0100

    move formating code to custom_formats.rs

--- src/formats.rs
+++ src/formats.rs
@@ -1,23 +1,16 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::HashMap,
     sync::OnceLock,
 };
 
-use chrono::{format::StrftimeItems, NaiveDate, NaiveDateTime, NaiveTime};
-
-use std::fmt::Write;
-
 use crate::{
-    custom_format::parse_custom_format,
+    custom_format::{parse_custom_format, format_custom_format_f64},
     datatype::DataTypeRef,
-    locales::{get_locale_symbols, get_time_locale},
     DataType,
 };
 
-const INVALID_VALUE: &'static str = "############";
-
 /// https://learn.microsoft.com/en-us/office/troubleshoot/excel/1900-and-1904-date-system
-static EXCEL_1900_1904_DIFF: i64 = 1462;
+pub(crate) static EXCEL_1900_1904_DIFF: i64 = 1462;
 
 fn get_builtin_formats() -> &'static HashMap<usize, CellFormat> {
     static INSTANCE: OnceLock<HashMap<usize, CellFormat>> = OnceLock::new();
@@ -135,7 +128,7 @@ impl Condition {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Fix {
-    fix_string: Option<String>,
+    pub fix_string: Option<String>,
 }
 
 impl Fix {
@@ -378,6 +371,7 @@ pub fn builtin_format_by_id(id: &[u8]) -> CellFormat {
 /// Check if code corresponds to builtin date format
 ///
 /// See `is_builtin_date_format_id`
+// FIXME
 pub fn builtin_format_by_code(code: u16) -> CellFormat {
     match code {
         14..=22 | 45 | 47 => CellFormat::DateTime,
@@ -405,416 +399,9 @@ pub fn format_excel_i64(value: i64, format: Option<&CellFormat>, is_1904: bool) 
             }) as f64,
         ),
         Some(CellFormat::TimeDelta) => DataType::Duration(value as f64),
-        Some(CellFormat::Custom(custom_format)) => todo!(),
+	// coercing i64 to f64 is ok because Excel store integers as double floats
+        Some(CellFormat::Custom(custom_format)) => format_custom_format_f64(value as f64, custom_format, is_1904).into(),
         _ => DataType::Int(value),
-    }
-}
-
-fn format_excell_date_time(f: f64, format: &str, locale: Option<usize>) -> Option<String> {
-    if f > 0.0 {
-        let Some(start) = NaiveDate::from_ymd_opt(1900, 1, 1) else {
-            return None;
-        };
-        let secs = 86400.0 * (f - f.floor());
-        let days = f as i64;
-
-        let Some(date) = start.checked_add_signed(chrono::Duration::days(days - 2)) else {
-            return None;
-        };
-        let Some(time) = NaiveTime::from_num_seconds_from_midnight_opt(secs as u32, 0) else {
-            return None;
-        };
-
-        let ndt = NaiveDateTime::new(date, time);
-
-        let dtl = ndt.and_utc();
-
-        let fmt = StrftimeItems::new(format);
-
-        let mut formatted_str = String::new();
-
-        if let Some(locale) = locale {
-            if let Some((_, ls)) = get_locale_symbols(locale) {
-                if let Ok(locale) = TryFrom::<&str>::try_from(*ls) {
-                    match write!(
-                        formatted_str,
-                        "{}",
-                        dtl.format_localized_with_items(fmt, locale).to_string()
-                    ) {
-                        Ok(_) => {
-                            return Some(formatted_str);
-                        }
-                        Err(_) => return None,
-                    }
-                }
-            }
-        }
-        if let Ok(_) = write!(
-            formatted_str,
-            "{}",
-            NaiveDateTime::from(ndt).format_with_items(fmt).to_string()
-        ) {
-            return Some(formatted_str);
-        }
-    }
-    // FIXME, should we try to format with default format if locale is present
-    // if locale is not present use en_US format ?
-    None
-}
-
-//FIXME, check is_1904 thing
-fn format_with_dformat(
-    value: f64,
-    format: &DFormat,
-    locale: Option<usize>,
-    is_1904: bool,
-) -> DataTypeRef<'static> {
-    let value = if is_1904 {
-        value + EXCEL_1900_1904_DIFF as f64
-    } else {
-        value
-    };
-
-    if let Some(s) = format_excell_date_time(value, &format.strftime_fmt, locale) {
-        return DataTypeRef::String(s);
-    }
-
-    DataTypeRef::DateTime(value)
-}
-
-// FIXME, we should do this without allocating
-fn format_with_fformat(mut value: f64, fformat: &FFormat, locale: Option<usize>) -> String {
-    // FIXME, dp limit ??
-    fn excell_round(value: f64, dp: i32) -> String {
-        let v = 10f64.powi(dp);
-        let value = (value * v).round() / v;
-        format!("{:.*}", dp as usize, value)
-    }
-
-    if fformat.ff_type == NumFormatType::Percentage {
-        value = value * 100.0;
-    }
-
-    let locale_data = locale.map_or(None, |i| get_time_locale(i));
-
-    // we are using en_US as default locale if not specified
-    let decimal_point = locale_data.map_or(".", |ld| ld.num_decimal_point);
-    let thousand_separator = locale_data.map_or(",", |ld| ld.num_thousands_sep);
-
-    let dec_places = fformat.significant_digits + fformat.insignificant_zeros;
-
-    let significant_digits = fformat.significant_digits;
-    let insignificant_zeros = fformat.insignificant_zeros;
-    let grouping_count = fformat.group_separator_count;
-
-    let mut str_value = excell_round(value, dec_places as i32);
-
-    if grouping_count == 0 && significant_digits == 0 {
-        if fformat.ff_type == NumFormatType::Percentage {
-            str_value.push('%');
-        }
-
-        // only if decimal point is not "."
-        if !decimal_point.eq(".") {
-            // chars().position() is OK since it's String representation of float value so it's ASCII only
-            if let Some(di) = str_value.chars().position(|c| c.eq(&'.')) {
-                let mut sb = str_value.into_bytes();
-                sb.splice(di..di + 1, decimal_point.bytes());
-                return String::from_utf8(sb).unwrap();
-            }
-        }
-        return str_value;
-    }
-
-    let chars_value: Vec<char> = str_value.chars().collect();
-    let dot_position = chars_value.iter().position(|x| (*x).eq(&'.'));
-
-    let mut value_decimal_places: i32 = if let Some(dot_position) = dot_position {
-        (chars_value.len() - dot_position - 1) as i32
-    } else {
-        0
-    };
-
-    let mut new_str_value: VecDeque<char> = VecDeque::new();
-
-    let mut dot = false;
-    let mut last_group = 0;
-    let mut nums = 0;
-
-    // if we don't have decimal places then dot is already passed
-    if value_decimal_places == 0 {
-        dot = true;
-    }
-
-    for (_, ch) in chars_value.iter().rev().enumerate() {
-        match (*ch, dot) {
-            ('0', false) => {
-                if value_decimal_places <= insignificant_zeros {
-                    new_str_value.push_front(*ch);
-                }
-            }
-            ('.', false) => {
-                // replacing it with localized dec seprator
-                for c in decimal_point.chars() {
-                    new_str_value.push_front(c);
-                }
-                dot = true;
-            }
-            (c, false) => {
-                new_str_value.push_front(c);
-            }
-            ('-', true) => {
-                new_str_value.push_front('-');
-            }
-            (c, true) => {
-                if grouping_count > 0 {
-                    if last_group == grouping_count {
-                        for c in thousand_separator.chars() {
-                            new_str_value.push_front(c);
-                        }
-                        last_group = 0;
-                    }
-                    last_group += 1;
-                }
-                new_str_value.push_front(c);
-                nums += 1;
-            }
-        }
-        if !dot {
-            value_decimal_places -= 1;
-        }
-    }
-
-    // feel front with zeros if we have more p_insignificant_zeros
-    if dot {
-        if nums < (fformat.p_insignificant_zeros) {
-            for _ in 0..(fformat.p_insignificant_zeros - nums) {
-                new_str_value.push_front('0');
-            }
-        }
-    } else {
-        // here we now that we don't have decimal place so we can count all digits
-        let vl: i32 = chars_value.len() as i32;
-        if vl < (fformat.p_significant_digits) {
-            for _ in 0..(fformat.p_insignificant_zeros - vl) {
-                new_str_value.push_front('0');
-            }
-        }
-    }
-
-    if fformat.ff_type == NumFormatType::Percentage {
-        new_str_value.push_back('%');
-    }
-
-    new_str_value.iter().collect()
-}
-
-fn format_num_format(
-    value: f64,
-    format: &FormatPart,
-    num_format: &NumFormat,
-) -> DataTypeRef<'static> {
-    let value = value;
-
-    let mut prefix = format.prefix.as_ref().map_or_else(
-        || "",
-        |fix| fix.fix_string.as_ref().map_or_else(|| "", |s| &s),
-    );
-
-    let mut suffix = format.suffix.as_ref().map_or_else(
-        || "",
-        |fix| fix.fix_string.as_ref().map_or_else(|| "", |s| &s),
-    );
-
-    let locale = format.locale;
-
-    let value = if let Some(ref fformat) = num_format.fformat {
-        format_with_fformat(value, fformat, locale)
-    } else {
-        String::from("")
-    };
-
-    // do not use prefix/suffix if just blank characters
-    prefix = if prefix.chars().all(char::is_whitespace) {
-        ""
-    } else {
-        prefix
-    };
-
-    suffix = if suffix.chars().all(char::is_whitespace) {
-        ""
-    } else {
-        suffix
-    };
-
-    DataTypeRef::String(format!("{}{}{}", prefix, value, suffix))
-}
-
-fn no_value_format(format: &FormatPart) -> DataTypeRef<'static> {
-    let mut prefix = format.prefix.as_ref().map_or_else(
-        || "",
-        |fix| fix.fix_string.as_ref().map_or_else(|| "", |s| &s),
-    );
-
-    let mut suffix = format.suffix.as_ref().map_or_else(
-        || "",
-        |fix| fix.fix_string.as_ref().map_or_else(|| "", |s| &s),
-    );
-
-    prefix = if prefix.chars().all(char::is_whitespace) {
-        ""
-    } else {
-        prefix
-    };
-
-    suffix = if suffix.chars().all(char::is_whitespace) {
-        ""
-    } else {
-        suffix
-    };
-
-    DataTypeRef::String(format!("{}{}", prefix, suffix))
-}
-
-pub fn format_part_format_f64(
-    value: f64,
-    format_part: Option<&FormatPart>,
-    is_1904: bool,
-) -> DataTypeRef<'static> {
-    let Some(format) = format_part else {
-        if value < 0.0 {
-            return DataTypeRef::String(String::from("-"));
-        } else {
-            return DataTypeRef::String(String::from(""));
-        }
-    };
-
-    let value_format = format.value.as_ref();
-
-    match value_format {
-        Some(ValueFormat::Number(ref nf)) => format_num_format(value, format, nf),
-        Some(ValueFormat::Date(ref df)) => format_with_dformat(value, df, format.locale, is_1904),
-        Some(ValueFormat::Text) => todo!(),
-        None => no_value_format(format),
-    }
-}
-
-pub fn format_custom_format_f64(
-    value: f64,
-    custom_format: &CustomFormat,
-    is_1904: bool,
-) -> DataTypeRef<'static> {
-    fn format_index_match(v: f64, format_index: usize, formats_count: usize) -> bool {
-        if v > 0.0 {
-            format_index == 0
-        } else if v < 0.0 {
-            format_index == 1
-        } else {
-            if formats_count == 2 {
-                format_index == 0
-            } else {
-                format_index == 2
-            }
-        }
-    }
-
-    fn value_match_format(
-        v: f64,
-        format: Option<&FormatPart>,
-        format_index: usize,
-        formats_count: usize,
-    ) -> bool {
-        if let Some(format) = format {
-            if let Some(ref condition) = format.condition {
-                condition.run_condition_f64(v)
-            } else {
-                format_index_match(v, format_index, formats_count)
-            }
-        } else {
-            format_index_match(v, format_index, formats_count)
-        }
-    }
-
-    // in some cases we need to use abs() of negative value
-    // case: when format doesn't have custom condition and it's default format meant for negative values (2. format)
-    fn maybe_abs_of_neg_value(value: f64, format: Option<&FormatPart>, format_index: usize) -> f64 {
-        if format_index != 1 {
-            return value;
-        }
-
-        let Some(format) = format else {
-            return value;
-        };
-
-        let Some(ref _cond) = format.condition else {
-            // if format doesn't have custom condition use abs()
-            return value.abs();
-        };
-
-        value
-    }
-
-    let formats_count = custom_format.formats.len();
-    let format_parts = &custom_format.formats;
-    let has_custom_conditions = format_parts
-        .iter()
-        .any(|f| f.as_ref().map_or(false, |ref f| f.condition.is_some()));
-
-    match formats_count {
-        0 => DataTypeRef::String(String::from(INVALID_VALUE)),
-        1 => {
-            return format_part_format_f64(value, format_parts[0].as_ref(), is_1904);
-        }
-        2 => {
-            if !has_custom_conditions {
-                if value >= 0.0 {
-                    return format_part_format_f64(value, format_parts[0].as_ref(), is_1904);
-                } else {
-                    return format_part_format_f64(value.abs(), format_parts[1].as_ref(), is_1904);
-                }
-            }
-
-            let fp_1 = format_parts[0].as_ref();
-            let fp_2 = format_parts[1].as_ref();
-
-            if value_match_format(value, fp_1, 0, 2) {
-                return format_part_format_f64(value, fp_1, is_1904);
-            } else {
-                if value_match_format(value, fp_2, 1, 2) {
-                    let v = maybe_abs_of_neg_value(value, fp_2, 1);
-                    return format_part_format_f64(v, fp_2, is_1904);
-                } else {
-                    // if both formats have custom condition then Excel prints ###########
-                    return DataTypeRef::String(String::from(INVALID_VALUE));
-                }
-            }
-        }
-        3 | 4 => {
-            if !has_custom_conditions {
-                if value > 0.0 {
-                    return format_part_format_f64(value, format_parts[0].as_ref(), is_1904);
-                } else if value < 0.0 {
-                    return format_part_format_f64(value.abs(), format_parts[1].as_ref(), is_1904);
-                } else {
-                    // zero
-                    return format_part_format_f64(value, format_parts[2].as_ref(), is_1904);
-                }
-            }
-
-            let fp_3 = format_parts[2].as_ref();
-
-            // only first two formats can have condition
-            for (i, f) in format_parts[0..2].iter().enumerate() {
-                if value_match_format(value, f.as_ref(), i, formats_count) {
-                    let v = maybe_abs_of_neg_value(value, f.as_ref(), i);
-                    return format_part_format_f64(v, f.as_ref(), is_1904);
-                }
-            }
-
-            // if first two doesn't match use third
-            return format_part_format_f64(value, fp_3, is_1904);
-        }
-        _ => return DataTypeRef::String(String::from(INVALID_VALUE)),
     }
 }
 
@@ -1162,10 +749,10 @@ fn test_is_date_format() {
 #[cfg(test)]
 mod test {
     use crate::{
-        custom_format::parse_custom_format,
+        custom_format::{parse_custom_format, format_with_fformat},
         datatype::DataTypeRef,
         formats::{
-            detect_custom_number_format, format_excel_f64_ref, format_with_fformat, FFormat,
+            detect_custom_number_format, format_excel_f64_ref, FFormat,
         },
     };
 
